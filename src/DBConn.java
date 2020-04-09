39d88163fd7f89da12e0e9dcfca35a33fc4e22a7
Haibot-Lee <18252125@life.hkbu.edu.hk>
2020-04-10 04:57:17 +0800

    make order 差不多写完了 SQL 里面table的属性有些我也改了 我改的地方实在很多不好解释，你改之前我们见面解释吧！

--- src/DBConn.java
+++ src/DBConn.java
@@ -1,18 +1,74 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DBConn {
 
-    public void orderMaking() {
+    public void login() {
 
+    }
+
+    public void orderMaking(String stuNo) {
+        //check whether he can make order or not
+        if (ifAddOrder(stuNo)) {
+            // create orderNo, orderDate
+            String orderNo = "";
+
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-YYYY");
+            String orderDate = dateFormat.format(date);
+
+            //let user choose pay method: 1. cash 2.octopus 3.credit card
+            String payMethod = "";
+            String cardNo;
+            if (payMethod.equals("credit card")) {
+                cardNo = "let user input here";
+                cardNo = "'" + cardNo + "'";
+            } else
+                cardNo = "NULL";
+
+            // use this SQL to insert order
+            String sql = "INSERT INTO ORDERS(order_no, stu_no, order_date, pay_method, card_no) VALUES(\'"
+                    + orderNo + "\',\'"
+                    + stuNo + "\',\'"
+                    + orderDate + "\',\'"
+                    + payMethod + "\',"
+                    + cardNo + ")";
+
+            // add books to order
+            boolean ifAddBook;
+            do {
+                ifAddBook = false;
+                addBook(orderNo);
+                //if continue add, ifAddBook -> true.
+
+            } while (ifAddBook == true);
+        }
+    }
+
+    private boolean ifAddOrder(String stuNo) {
+
+        return true;
+    }
+
+    private void addBook(String orderNo) {
+        //let user choose book and quantity
+        String bookNo = "";
+        int qty = 1;
+
+        // use this SQL to insert book
+        String sql = "INSERT INTO ORDERS(order_no, book_no, qty) VALUES(\'"
+                + orderNo + "\',\'" + bookNo + "\'," + qty + ")";
     }
 
     public void orderSearch() {
 
     }
 
-    public void orderUpdate(){
+    public void orderUpdate() {
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
