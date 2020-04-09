e0644934132b9fbf27ce2183e5b721ad54253eb0
Haibot-Lee <18252125@life.hkbu.edu.hk>
2020-04-09 15:12:05 +0800

    SQL update

---
+++ sql/group3_dbinsert.sql
@@ -0,0 +1,48 @@
+CREATE TABLE BOOKS(
+ book_no CHAR(13),
+ title 	VARCHAR(50) NOT NULL,
+ author	VARCHAR(30) NOT NULL,
+ price	REAL NOT NULL,
+ amount	INT NOT NULL,
+ PRIMARY KEY (book_no));
+
+CREATE TABLE STUDENTS(
+ stu_no CHAR(8),
+ name VARCHAR(30) NOT NULL,
+ gender CHAR(1) NOT NULL,
+ major VARCHAR(20) NOT NULL,
+ total_order REAL,
+ discount REAL,
+ PRIMARY KEY (stu_no));
+
+CREATE TABLE BOOK_IN_ORDERS(
+ order_no CHAR(),
+ book_no CHAR(13),
+ qty INT,
+ delivered INT,
+ PRIMARY KEY (order_no, book_no),
+ FOREIGN KEY (book_no) REFERENCES BOOKS ON DELETE CASCADE);
+ 
+CREATE TABLE ORDERS(
+ order_no CHAR(),
+ stu_no CHAR(8),
+ order_date DATE NOT NULL,
+ status INT,
+ total_price REAL,
+ pay_method VARCHAR(30) NOT NULL,
+ card_no CHAR(16),
+ PRIMARY KEY (order_no),
+ FOREIGN KEY(stu_no) REFERENCES STUDENTS ON DELETE CASCADE,
+ );
+
+
+CREATE OR REPLACE TRIGGER students_constraint
+BEFORE INSERT OR UPDATE ON STUDENTS
+FOR EACH ROW
+BEGIN
+    IF (:new.gender != 'M' AND :new.gender != 'm' AND :new.gender != 'F' AND :new.gender != 'f') THEN
+        RAISE_APPLICATION_ERROR(-20000, 'invalid gender');
+    END IF;
+END;
+.
+/
