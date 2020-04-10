f4353fd9b789170e58a8523ac2ae033252421cce
Haibot-Lee <18252125@life.hkbu.edu.hk>
2020-04-10 18:00:30 +0800

    SQL 更新 +default values + comment

--- sql/group3_dbinsert.sql
+++ sql/group3_dbinsert.sql
@@ -24,7 +24,7 @@ CREATE TABLE ORDERS
     order_no    CHAR(10),
     stu_no      CHAR(8)     NOT NULL,
     order_date  DATE        NOT NULL,
-    status      INT  DEFAULT 0,
+    status      INT  DEFAULT 0, -- 0未下单，1已下单(可取消)，2不可取消，3完成， 4已取消
     total_price REAL DEFAULT 0,
     pay_method  VARCHAR(30) not null,
     card_no     CHAR(16),
@@ -78,9 +78,7 @@ CREATE OR REPLACE TRIGGER del_dislevel&amount_constraint
     ON ORDERS
     FOR EACH ROW
 DECLARE
-    c   REAL;
-    cnt INT;
-    i INT;
+    c REAL;
 BEGIN
     UPDATE STUDENTS SET total_order = total_order - :old.total_price WHERE stu_no = :old.stu_no;
     SELECT total_order INTO c FROM STUDENTS WHERE stu_no = :old.stu_no;
@@ -93,13 +91,6 @@ END;
 .
 /
 
-INSERT INTO STUDENTS
-VALUES ('11111111', 'Kurt', 'M', 'COMP', 1900, 0.1);
-INSERT INTO STUDENTS
-VALUES ('22222222', 'Rex', 'M', 'COMP', 900, 0);
-INSERT INTO STUDENTS
-VALUES ('33333333', 'Jerry', 'M', 'COMP', 300, 0);
-
 -- Calculating total_price of an Order
 CREATE OR REPLACE TRIGGER total_price_insert
     AFTER INSERT
@@ -110,15 +101,18 @@ DECLARE
     d REAL;
 BEGIN
     SELECT price INTO p FROM BOOKS WHERE BOOKS.book_no = :new.book_no;
-    SELECT discount INTO d
-        FROM ORDERS O NATURAL JOIN STUDENTS S WHERE order_no = :new.order_no;
+    SELECT discount
+    INTO d
+    FROM ORDERS O
+             NATURAL JOIN STUDENTS S
+    WHERE order_no = :new.order_no;
     UPDATE ORDERS SET total_price = total_price + :new.qty * p * (1 - d) WHERE order_no = :new.order_no;
 END;
 .
 /
 
 CREATE OR REPLACE TRIGGER total_price_update
-    AFTER UPDATE    -- when a new book is already in the order
+    AFTER UPDATE -- when a new book is already in the order
     ON BOOK_IN_ORDERS
     FOR EACH ROW
 DECLARE
@@ -126,15 +120,18 @@ DECLARE
     d REAL;
 BEGIN
     SELECT price INTO p FROM BOOKS WHERE BOOKS.book_no = :new.book_no;
-    SELECT discount INTO d
-        FROM ORDERS O NATURAL JOIN STUDENTS S WHERE order_no = :new.order_no;
+    SELECT discount
+    INTO d
+    FROM ORDERS O
+             NATURAL JOIN STUDENTS S
+    WHERE order_no = :new.order_no;
     UPDATE ORDERS SET total_price = total_price + (:new.qty - :old.qty) * p * (1 - d) WHERE order_no = :new.order_no;
 END;
 .
 /
 
 CREATE OR REPLACE TRIGGER total_price_delete
-    AFTER DELETE    -- TODO Review the DELETE condition later
+    AFTER DELETE -- TODO Review the DELETE condition later
     ON BOOK_IN_ORDERS
     FOR EACH ROW
 DECLARE
@@ -142,9 +139,39 @@ DECLARE
     d REAL;
 BEGIN
     SELECT price INTO p FROM BOOKS WHERE BOOKS.book_no = :old.book_no;
-    SELECT discount INTO d
-        FROM ORDERS O NATURAL JOIN STUDENTS S WHERE order_no = :old.order_no;
+    SELECT discount
+    INTO d
+    FROM ORDERS O
+             NATURAL JOIN STUDENTS S
+    WHERE order_no = :old.order_no;
     UPDATE ORDERS SET total_price = total_price - :old.qty * p * (1 - d) WHERE order_no = :old.order_no;
 END;
 .
 /
+
+
+INSERT INTO STUDENTS
+VALUES ('11111111', 'Kurt', 'M', 'COMP', 1600, 0.1);
+INSERT INTO STUDENTS
+VALUES ('22222222', 'Rex', 'M', 'COMP', 300, 0);
+INSERT INTO STUDENTS
+VALUES ('33333333', 'Jerry', 'M', 'COMP', 300, 0);
+
+INSERT INTO BOOKS
+VALUES ('001', 'Harry Potter I', 'J. K. Rowling', 300, 11);
+INSERT INTO BOOKS
+VALUES ('002', 'Harry Potter II', 'J. K. Rowling', 300, 2);
+INSERT INTO BOOKS
+VALUES ('003', 'Harry Potter III', 'J. K. Rowling', 400, 20);
+
+INSERT INTO ORDERS
+VALUES ('222222221', '22222222', '02-APR-2000', 2, 600, 'Cash', '123456789');
+INSERT INTO ORDERS
+VALUES ('111111111', '11111111', '07-APR-2000', 2, 300, 'Cash', '987654321');
+
+INSERT INTO BOOK_IN_ORDERS
+VALUES ('222222221', '001', 1, '9-APR-2000');
+INSERT INTO BOOK_IN_ORDERS
+VALUES ('222222221', '002', 1, NULL);
+INSERT INTO BOOK_IN_ORDERS
+VALUES ('111111111', '002', 1, NULL);
