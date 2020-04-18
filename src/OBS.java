9028dc34fd2f6f712208ddcc9c02215178bf819a
Haibot-Lee <18252125@life.hkbu.edu.hk>
2020-04-18 21:37:23 +0800

    GUI update

--- src/OBS.java
+++ src/OBS.java
@@ -2,38 +2,42 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OBS {
+    DBConn db = new DBConn("e8252125", "e8252125");
 
     public OBS() {
-        DBConn db = new DBConn("e8252125", "e8252125");
-        JFrame homePage=new JFrame();
-        homePage.setTitle("Online University Bookshop");
-        homePage.setSize(800, 400);
+        List<String> sids = db.selectSid();
+//        List<String> sids = new ArrayList<>();
+//        sids.add("1");
+
+        JFrame homePage = new JFrame("Online University Bookshop");
+        homePage.setSize(600, 400);
         homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container c = homePage.getContentPane();
-        c.setLayout(new GridLayout(8,1));
+        c.setLayout(new GridLayout(3, 1,20,20));
         c.add(new JLabel("Welcome to Online University Bookshop! Please choose one function!", SwingConstants.CENTER));
 
         Button b1 = new Button("Order Search");
         Button b2 = new Button("Order Making");
-        b1.setBounds(50,300,200,50);
-        b1.setBounds(200,300,100,50);
+        b1.setBounds(50, 300, 200, 50);
+        b1.setBounds(200, 300, 100, 50);
         c.add(b1);
         c.add(b2);
 
         b1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String[] students={"123"};
-                String sid=login(students);
+                String sid = login(sids);
                 orderSearching(sid);
+
             }
         });
 
         b2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String[] students={"123"};
-                String sid=login(students);
+                String sid = login(sids);
                 orderMaking(sid);
             }
         });
@@ -41,22 +45,14 @@ public class OBS {
         homePage.setVisible(true);
     }
 
-    public void orderSearching(String sid){
-
-    }
-
-    public void orderMaking(String sid){
-
-    }
-
-    public String login(String[] students) {
+    public String login(List<String> sids) {
         boolean ifexist = false;
         String sid;
         do {
             JPanel panel = new JPanel();
             final TextField sidField = new TextField();
             panel.setLayout(new GridLayout(2, 1));
-            panel.add(new JLabel("Please input your student ID:"));
+            panel.add(new JLabel("Please login with your student ID:"));
             panel.add(sidField);
 
             JOptionPane pane = new JOptionPane(panel) {
@@ -71,7 +67,7 @@ public class OBS {
 
             sid = sidField.getText();
 
-            for (String i : students) {
+            for (String i : sids) {
                 if (i.equals(sid)) {
                     ifexist = true;
                     break;
@@ -85,6 +81,90 @@ public class OBS {
         } while (!ifexist);
 
         return sid;
+    }
+
+    public void orderMaking(String sid) {
+        List<Order> orders = db.searchOrder(sid);
+        for (Order i : orders) {
+            if (i.status == 0 || i.status == 1) {
+                System.out.println("You have outstanding orders!");
+                return;
+            }
+        }
+
+        payMethod();
+
+        JFrame omPage = new JFrame("Order Making");
+        omPage.setSize(800, 1000);
+        omPage.setLayout(new GridLayout(1, 2));
+        Container omc = omPage.getContentPane();
+        JTextArea orderInfo = new JTextArea();
+        orderInfo.setSize(400, 1000);
+
+        String books = "1111\n2222\n3333\n4444";
+        //将books的信息转换为string --TODO
+
+        orderInfo.setText(books);
+        omc.add(orderInfo);
+
+        Panel addbooks = new Panel();
+        Button b1 = new Button("Add");
+        Button b2 = new Button("Confirm");
+        Button b3 = new Button("Cancel");
+        b1.setBounds(650, 50, 100, 40);
+        b2.setBounds(650, 150, 50, 40);
+        b3.setBounds(650, 250, 50, 40);
+
+        omPage.setVisible(true);
+    }
+
+    public void payMethod() {
+
+    }
+
+    public void orderSearching(String sid) {
+        JFrame osPage = new JFrame("All of your orders");
+        osPage.setLayout(null);
+        osPage.setSize(800, 1000);
+        Container osc = osPage.getContentPane();
+        JTextArea orderInfo = new JTextArea();
+        orderInfo.setBounds(0, 0, 600, 1000);
+
+        String orders = "1111\n2222\n3333\n4444";
+        //将order的信息转换为string --TODO
+
+        orderInfo.setText(orders);
+        osc.add(orderInfo);
+
+        Button b1 = new Button("Order Update");
+        Button b2 = new Button("Order Cancelling");
+        b1.setBounds(650, 50, 100, 40);
+        b2.setBounds(650, 150, 100, 40);
+        osc.add(b1);
+        osc.add(b2);
+
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                orderUpdate(sid);
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                orderCancelling(sid);
+            }
+        });
+
+        osPage.setVisible(true);
+
+    }
+
+    public void orderUpdate(String sid) {
+
+    }
+
+    public void orderCancelling(String sid) {
+
     }
 
     public static void main(String[] args) {
